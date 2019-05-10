2a912296bc0c26f654f6a93a928f3a99bc9c0dfe
Jungtaek Kim <judaigi@gmail.com>
2019-05-10 23:59:46 +0900

    레벨 구성용 Sprite과 캐릭터, 몬스터 Sprite 추가. 레벨 1과 레벨 2 구성.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    GameObject player;
+
+    void Awake()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        transform.position = player.transform.position + new Vector3(0f, 0f, -10f);
+    }
+}
